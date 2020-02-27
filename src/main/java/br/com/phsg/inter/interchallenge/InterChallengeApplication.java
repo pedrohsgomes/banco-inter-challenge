d75b39fa971d64df98595c425843c8fdc523fce4
Patricia PA. Amaral <patricia.amaral@camargosjunior.local>
2020-02-27 09:28:26 -0300

    Inicial commit, projeto limpo

---
+++ src/main/java/br/com/phsg/inter/interchallenge/InterChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.phsg.inter.interchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/phsg/inter/interchallenge/InterChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phsg.inter.interchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
